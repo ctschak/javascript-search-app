643062519527a8cd94e3f63e4fadcd58856679a5
Chakravarthy Ravichandran <surathy2003@gmail.com>
2017-08-20 13:11:58 -0700

    First version of autocomplete impl

---
+++ README.md
@@ -0,0 +1,44 @@
+# Javascript Search SPA
+
+## **Initial/First Time Set-up**
+
+##### **Step 1:**
+Clone the repository Get BitBucket_URL from top right HTTPS field.
+
+```
+#!git
+git clone https://github.com/ctschak/javascript-search-app.git
+```
+
+##### **Step 2:**
+Install dependencies
+
+```
+#!git
+npm install
+```
+##### **Step 3:**
+Start the application
+
+```
+#!git
+npm start
+```
+
+##### **Step 4:**
+Access the application using http://localhost:7000
+
+## **Other npm commands**
+
+#### 1. Clean your node/bower installations
+```
+#!git
+npm run clean
+```
+
+#### 2. Deep Clean and install - this will clean npm cache
+
+```
+#!git
+npm run clean-install
+```
--- src/app/modules/search/search.controller.js
+++ src/app/modules/search/search.controller.js
@@ -1,5 +1,18 @@
-spaApp.controller('searchCtrl', function ($scope, $rootScope, $log, $state, $stateParams) {
+spaApp.controller('searchCtrl', function ($scope, $rootScope, $log, $state, $stateParams, SearchService, $q, $filter) {
     'use strict';
     $log.info('+ searchCtrl()');
     $scope.dashTest = 'TEST';
+    $scope.search = function(text) {
+      $log.debug("Inside search = " + text);
+      var deferred =  $q.defer();
+      var payload = {'q': text};
+      if (text !== '' && text !== undefined) {
+        SearchService.searchFinTech(payload, function (data) {
+          $log.info("data" + JSON.stringify(data));
+          var filteredProds = $filter('filter')(data.products, text);
+          return deferred.resolve(filteredProds);
+        });
+      }
+      return deferred.promise;
+    }
 });
